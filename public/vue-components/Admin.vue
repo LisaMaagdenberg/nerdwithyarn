00439b72b89d3202e67fcd74863c4de53fcfcb0d
LisaMaagdenberg <lisamaagdenberg@MacBook-Pro-van-lisa.local>
2018-03-25 14:23:31 +0200

    footer works, scss divided

--- public/vue-components/Admin.vue
+++ public/vue-components/Admin.vue
@@ -1,17 +1,20 @@
 <template>
-  <div>
+  <div class="flex-parent">
     <Menu></Menu>
     <div class="page">
       <h1>Admin</h1>
     </div>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
 import Menu from './onEachPage/Menu.vue';
+import Footer from './onEachPage/Footer.vue';
 export default {
   components: {
-    Menu
+    Menu,
+    Footer
   }
 }
 </script>
